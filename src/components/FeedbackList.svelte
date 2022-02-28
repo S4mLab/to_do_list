51e264592da233860c16a111ddc605ff8715f96b
sam <samvbui1995@gmail.com>
2022-02-28 23:24:27 +1100

    basic svelte to do list

---
+++ src/components/FeedbackList.svelte
@@ -0,0 +1,8 @@
+<script>
+    import FeedbackItem from './FeedbackItem.svelte'
+    export let feedbackObjList = [];
+</script>
+
+{#each feedbackObjList as feedbackObj (feedbackObj.id)}
+    <FeedbackItem {feedbackObj}/>
+{/each}
