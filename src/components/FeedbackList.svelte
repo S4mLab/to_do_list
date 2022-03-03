f0e2b161e41657f69a3ca4bf30732aad3e827dc6
sam <samvbui1995@gmail.com>
2022-03-03 16:30:23 +1100

    learn about component dispatch custom event

--- src/components/FeedbackList.svelte
+++ src/components/FeedbackList.svelte
@@ -1,8 +1,13 @@
 <script>
     import FeedbackItem from './FeedbackItem.svelte'
     export let feedbackObjList = [];
+
+    // send feedback items isn't imbeded in FeedbackList 
+    // we can call the event 'delete-feedback' from FeedbackItem
+
 </script>
 
 {#each feedbackObjList as feedbackObj (feedbackObj.id)}
-    <FeedbackItem {feedbackObj}/>
+    <FeedbackItem {feedbackObj} on:delete-feedback/>
 {/each}
+
