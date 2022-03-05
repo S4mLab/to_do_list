fe625a82e32057f0601836a566b8fd1e9027815a
sam <samvbui1995@gmail.com>
2022-03-05 12:41:29 +1100

    adding form and validate input feature

---
+++ src/components/FeedbackForm.svelte
@@ -0,0 +1,71 @@
+<script>
+    import Button from './Button.svelte';
+    import Card from './Card.svelte';
+
+    let text = '';
+    let btnDisable = true;
+    let message;
+    const minTextLength = 10;
+    
+
+    const handleInput = () => {
+        if (text.trim().length <= minTextLength) {
+            message = `Text must be at least ${minTextLength} characters`;
+            btnDisable = true;
+        } else {
+            message = null
+            btnDisable = false;
+        }
+    }
+</script>
+
+<Card>
+    <header>
+        <h2>How would you rate your service with us?</h2>
+    </header>
+    <form>
+        <div class="input-group">
+            <input type="text" on:input={handleInput} bind:value={text} placeholder="Tell us something that keeps you coming back!">
+            <Button disable={btnDisable} type="submit"> Send </Button>
+        </div>
+        {#if message}
+            <div class="message">
+                {message}
+            </div>
+        {/if}
+    </form>
+</Card>
+
+
+<style>
+  header {
+    max-width: 400px;
+    margin: auto;
+  }
+  header h2 {
+    font-size: 22px;
+    font-weight: 600;
+    text-align: center;
+  }
+  .input-group {
+    display: flex;
+    flex-direction: row;
+    border: 1px solid #ccc;
+    padding: 8px 10px;
+    border-radius: 8px;
+    margin-top: 15px;
+  }
+  input {
+    flex-grow: 2;
+    border: none;
+    font-size: 16px;
+  }
+  input:focus {
+    outline: none;
+  }
+  .message{
+    padding-top: 10px;
+    text-align: center;
+    color: rebeccapurple;
+  }
+</style>
